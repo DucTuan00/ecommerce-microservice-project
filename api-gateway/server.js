70631ad4d98e04bfe69e352679f5d5dd7454b98f
DucTuan00 <tuan03062003@gmail.com>
2024-12-06 22:57:46 +0700

    thêm tính năng cho api-gateway

---
+++ api-gateway/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: true, 
+    credentials: true 
+}));
+
+app.use('/api/auth', createProxyMiddleware({ 
+    target: 'http://localhost:3001/api/auth', 
+    changeOrigin: true,
+}));
+app.use('/api/user', createProxyMiddleware({ 
+    target: 'http://localhost:3002/api/user', 
+    changeOrigin: true,
+}));
+
+app.listen(3000, () => {
+    console.log('API Gateway running on http://localhost:3000');
+});
