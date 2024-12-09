9894c44552dc8c22f6ac9ff5128dd2976f4337a6
DucTuan00 <tuan03062003@gmail.com>
2024-12-09 23:52:39 +0700

    thêm tính năng cho cart-service

---
+++ cart-service/routes/cartRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const cartController = require('../controllers/cartController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/', authMiddleware(['2']), cartController.createCart);
+
+router.put('/:id', authMiddleware(['2']), cartController.updateCart);
+
+router.delete('/:id', authMiddleware(['2']), cartController.deleteCart);
+
+router.get('/', authMiddleware(['2']), cartController.getCart);
+
+module.exports = router;
